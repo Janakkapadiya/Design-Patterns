d0cfc9087ff1cc696383c23115e33fef7a11a376
janak kapadiya <94526673+Janakkapadiya@users.noreply.github.com>
2022-12-28 17:47:15 +0530

    Delete Polymorphisam directory

--- Polymorphisam/constructorOverRiding/Lion.java
+++
@@ -1,9 +0,0 @@
-package Polymorphisam.constructorOverRiding;
-
-public class Lion extends Tiger{
-    @Override
-    void Strong(String data)
-    {
-        System.out.println("ohh yeah well i am " + data + " and i m fuckin king");
-    }
-}
